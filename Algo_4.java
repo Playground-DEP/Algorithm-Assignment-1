93b34e32b4cb8337349ae7c3da389909b08e11b4
Yashoda <gihany4@gmail.com>
2023-08-13 10:22:40 +0530

    Editing Algo_3

---
+++ Algo_4.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Algo_4 {
+    private final static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+        
+    }
+}
