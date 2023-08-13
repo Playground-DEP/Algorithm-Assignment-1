aed55ea4283f2ba99845a642f58f83274d213deb
Yashoda <gihany4@gmail.com>
2023-08-13 16:06:15 +0530

    Algo_9 completed

--- Algo_9.java
+++ Algo_9.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Algo_9 {
+    private final static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        
+        do{
+            System.out.print("Enter a word: ");
+            String word = scanner.nextLine().strip();
+            if (word.isBlank()) System.out.println("Please enter a word!");
+            else if (word.split(" ").length != 1) System.out.println("More than one word entered! please try again");
+            else{
+                boolean isPalindrome = true;
+                int length = word.length();
+                for (int i = 0; i < length/2; i++) {
+                    if (word.charAt(i) != word.charAt(word.length()-i-1)) isPalindrome = false;
+                    
+                }
+                if (isPalindrome) System.out.printf("%s is a palindrome\n", word);
+                else System.out.printf("%s is not a palindrome\n", word);
+            }
+        }while(true);
     }
 }
